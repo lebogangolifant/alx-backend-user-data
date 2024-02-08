481d718d724d48c0d2bde8127cc381269b7979c4
Lebogang <lebogangodeveloper@gmail.com>
2024-02-08 09:51:08 +0000

    Update RedactingFormatter class
    - to accept a list of strings fields constructor argument.
    - Implemented the format method to filter values in incoming log,
    records using filter_datum. Values for fields in fields should be filtered.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ filtered_logger module  that returns the log message obfuscated
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -17,3 +18,34 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(rf"{re.escape(field)}=(.*?){re.escape(separator)}",
                          f"{field}={redaction}{separator}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Formats the log record
+        """
+        for field in self.fields:
+            record.msg = self.filter_datum(field, self.REDACTION,
+                                           record.msg, self.SEPARATOR)
+        return super().format(record)
+
+    def filter_datum(self, field: str, redaction: str,
+                     message: str, separator: str) -> str:
+        """
+        Redacts specified field values in a log message
+        """
+        return re.sub(fr'{field}=(.*?){re.escape(separator)}',
+                      f'{field}={redaction}{separator}', message)
