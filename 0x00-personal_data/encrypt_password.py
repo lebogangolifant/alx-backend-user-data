f3d7229fbfc05798893403b5f30445ab58cd1617
Lebogang <lebogangodeveloper@gmail.com>
2024-02-08 12:18:34 +0000

    Check valid password
    - Implemented is_valid function to encrypt_password.py,
    - which securely validates passwords against hashed passwords
    using the bcrypt package

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -13,3 +13,10 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates the password against the hashed password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
