5f631fd0c247f7b0bcbb052eb2276cfc71c18b01
Lebogang <lebogangodeveloper@gmail.com>
2024-02-08 11:44:13 +0000

    Hashes the password using bcrypt
    - Implemented hash_password function for secure password hashing

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Passwords encryption module
+"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes the password using bcrypt
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
