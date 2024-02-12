918528cd83c33d6a965d5384895768220dc83670
Lebogang <lebogangodeveloper@gmail.com>
2024-02-12 12:10:06 +0000

    Add Unauthorized Error Handler and Endpoint
    - Added a new error handler to handle unauthorized requests with status code 401
    - Created a new endpoint /api/v1/unauthorized in index.py to trigger a 401 error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_endpoint() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - Unauthorized error
+    """
+    abort(401)
