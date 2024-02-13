ae9bc7a5db0ca5858548af8b969919a718893f17
Lebogang <lebogangodeveloper@gmail.com>
2024-02-13 10:08:25 +0000

    Implement Base64 Authorization Header Decoding in BasicAuth
    - Added decode_base64_authorization_header method to BasicAuth class to decode Base64 authorization headers
    - Implemented checks to handle various scenarios, such as None input, non-string input, and invalid Base64 format

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 BasicAuth module for the API
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -17,3 +18,15 @@ class BasicAuth(Auth):
            authorization_header.startswith('Basic '):
             return None
         return authorization_header.split(' ')[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header:
+                                           str) -> str:
+        """ Decode Base64 authorization header """
+        if base64_authorization_header is None or not \
+           isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode('utf-8')
+        except base64.binascii.Error:
+            return None
