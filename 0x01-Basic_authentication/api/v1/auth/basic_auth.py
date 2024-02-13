c4979cebd10a1bde6874365b56b7fa54e1d912b6
Lebogang <lebogangodeveloper@gmail.com>
2024-02-13 10:17:20 +0000

    Implement BasicAuth method extract_user_credentials
    - Implemented the extract_user_credentials method.
    - The method extracts the user email and password from the decoded Base64 authorization header.
    - Returns a tuple containing the email and password extracted.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -30,3 +30,16 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except base64.binascii.Error:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """
+        Extract user email and password from decoded Base64
+        authorization header
+        """
+        if decoded_base64_authorization_header is None or not \
+           isinstance(decoded_base64_authorization_header,
+                      str) or ':' not in decoded_base64_authorization_header:
+            return None, None
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
