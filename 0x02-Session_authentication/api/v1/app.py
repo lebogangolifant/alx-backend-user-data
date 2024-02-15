bcc5fa44009bb5439c10df89c1ea774943920e28
Lebogang <lebogangodeveloper@gmail.com>
2024-02-15 08:44:10 +0000

    Update @app.before_request method to exclude /api/v1/auth_session/login/ and check for both authorization header and session cookie

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,12 +32,16 @@ def before_request():
     """ Before request handler """
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     if auth:
         if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None and \
+               auth.session_cookie(request) is None:
                 abort(401)
-            request.current_user = auth.current_user(request)
+            # request.current_user = auth.current_user(request)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 @app.errorhandler(404)
