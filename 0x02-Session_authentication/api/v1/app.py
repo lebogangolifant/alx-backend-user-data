38bda8d20bff5702d091d5247e35cd98c925bbcd
Lebogang <lebogangodeveloper@gmail.com>
2024-02-16 06:40:41 +0000

    Fix import error
    Removed the import auth and directly assigns auth based on the value of auth_type

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -12,6 +12,7 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.session_auth import SessionAuth
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 
 app = Flask(__name__)
@@ -22,14 +23,12 @@ auth = None
 auth_type = getenv("AUTH_TYPE")
 if auth_type:
     if auth_type == 'auth':
-        from api.v1.auth.auth import Auth
         auth = Auth()
     elif auth_type == 'session_auth':
         auth = SessionAuth()
     elif auth_type == 'session_exp_auth':
         auth = SessionExpAuth()
     elif auth_type == 'session_db_auth':
-        from api.v1.auth.session_db_auth import SessionDBAuth
         auth = SessionDBAuth()
     else:
         auth = BasicAuth()
