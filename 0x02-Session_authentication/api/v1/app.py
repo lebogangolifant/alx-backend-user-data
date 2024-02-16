e5a00dd0a1e42dffe9e4985c2073244cb2dd76bf
Lebogang <lebogangodeveloper@gmail.com>
2024-02-16 08:05:25 +0000

    Fix import error
    - Updated the import statement to correctly import 'auth' from the appropriate location.

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,11 +7,7 @@ from os import getenv
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 from api.v1.views import app_views
-from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.session_auth import SessionAuth
-from api.v1.auth.session_exp_auth import SessionExpAuth
-from api.v1.auth.session_db_auth import SessionDBAuth
+import os
 
 
 app = Flask(__name__)
@@ -19,18 +15,22 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
-auth_type = getenv("AUTH_TYPE")
-if auth_type:
-    if auth_type == 'auth':
-        auth = Auth()
-    elif auth_type == 'session_auth':
-        auth = SessionAuth()
-    elif auth_type == 'session_exp_auth':
-        auth = SessionExpAuth()
-    elif auth_type == 'session_db_auth':
-        auth = SessionDBAuth()
-    else:
-        auth = BasicAuth()
+
+if getenv("AUTH_TYPE") == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif getenv("AUTH_TYPE") == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+elif getenv("AUTH_TYPE") == 'session_exp_auth':
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif getenv("AUTH_TYPE") == 'session_db_auth':
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.before_request
