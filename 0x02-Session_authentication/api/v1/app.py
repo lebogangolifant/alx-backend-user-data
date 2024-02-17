08dc89a57304042046e460a5afd90589aa6698ed
Lebogang <lebogangodeveloper@gmail.com>
2024-02-17 09:51:26 +0000

    Import module

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -7,29 +7,29 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
-
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-auth = None
 
-if getenv("AUTH_TYPE") == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-elif getenv("AUTH_TYPE") == 'session_auth':
-    from api.v1.auth.session_auth import SessionAuth
-    auth = SessionAuth()
-elif getenv("AUTH_TYPE") == 'session_exp_auth':
-    from api.v1.auth.session_exp_auth import SessionExpAuth
-    auth = SessionExpAuth()
-elif getenv("AUTH_TYPE") == 'session_db_auth':
-    from api.v1.auth.session_db_auth import SessionDBAuth
-    auth = SessionDBAuth()
-else:
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+auth = None
+auth_type = getenv("AUTH_TYPE")
+if auth_type:
+    if auth_type == 'basic_auth':
+        auth = BasicAuth()
+    elif auth_type == 'session_auth':
+        auth = SessionAuth()
+    elif auth_type == 'session_exp_auth':
+        auth = SessionExpAuth()
+    elif auth_type == 'session_db_auth':
+        auth = SessionDBAuth()
+    else:
+        auth = Auth()
 
 
 @app.before_request
@@ -38,7 +38,7 @@ def before_request():
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
                       '/api/v1/forbidden/',
-                      '/api/v1/auth_session/login/'] 
+                      '/api/v1/auth_session/login/']
     if auth:
         if auth.require_auth(request.path, excluded_paths):
             if auth.authorization_header(request) is None and \
