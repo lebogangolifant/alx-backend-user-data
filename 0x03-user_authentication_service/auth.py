1da7d963dd31702d3989c6fe1c988c976281b60c
Lebogang <lebogangodeveloper@gmail.com>
2024-02-19 12:41:11 +0000

    Implement _hash_password method in Auth module
    - Added the _hash_password method to the Auth module
    to generate a salted hash of the input password using bcrypt.hashpw.

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Auth module
+"""
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """
+    Hashes a password string using bcrypt
+    """
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
