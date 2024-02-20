84fd987d5c15d3f18d0a5bc53f85ac7a9823faed
Lebogang <lebogangodeveloper@gmail.com>
2024-02-20 13:09:23 +0000

    Implement Auth.create_session method
    - The method creates a new session for the user specified by the email argument.
    It generates a new UUID as the session ID,
    updates the user's session ID in the database, and returns the generated session ID.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -40,7 +40,7 @@ class Auth:
 
         try:
             self._db.find_user_by(email=email)
-            raise ValueError(f"User {email} already exists")
+            return f"User {email} already exists"
         except NoResultFound:
             pass
 
@@ -56,6 +56,20 @@ class Auth:
 
         try:
             user = self._db.find_user_by(email=email)
-            return checkpw(password.encode('utf-8'), user.hashed_password)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """
+        Create a session for the user
+        """
+        try:
+            found_user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return None
+
+        session_id = _generate_uuid()
+        self._db.update_user(found_user.id, session_id=session_id)
+        return session_id
