45372f933cd6e9233780221d65629dec854a549f
Lebogang <lebogangodeveloper@gmail.com>
2024-02-21 08:33:04 +0000

    Get reset password token
    - Implemented the get_reset_password_token function to handle
    the POST /reset_password route in the Flask app.
    - The function checks if the provided email is registered, generates
    a reset token if the email is registered, and returns the JSON response with status codes.

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -90,5 +90,18 @@ def profile():
     return jsonify({"email": email}), 200
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """
+    Creates a function to respond to the POST /reset_password route.
+    """
+    email = request.form.get('email')
+    try:
+        reset_token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        return jsonify({"error": "Email not registered"}), 403
+    return jsonify({"email": email, "reset_token": reset_token}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
